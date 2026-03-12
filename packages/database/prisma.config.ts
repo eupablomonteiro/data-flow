92f4682d19de99472818aa6fcb7ee9666a949d68
eupablomonteiro <pablo.monteirorgt19@gmail.com>
2026-03-12 04:54:24 -0300

    chore: initial monorepo setup with turborepo, prisma, api, worker and web.

---
+++ packages/database/prisma.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@prisma/config";
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env"),
+});
+
+export default defineConfig({
+  schema: "./prisma/schema.prisma",
+
+  migrations: {
+    path: "./prisma/migrations",
+  },
+
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
