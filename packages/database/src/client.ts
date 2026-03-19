3b0e6e08db04e0ba516bf8de74d2ca6f71de15ed
eupablomonteiro <pablo.monteirorgt19@gmail.com>
2026-03-19 00:25:17 -0300

    feat: Implementing persistence of upload metrics for visible, real-time feedback on the process for the user.

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -12,5 +12,3 @@ export const prisma = global.prisma ?? new PrismaClient({ adapter });
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
-
-export { UploadStatus } from "@prisma/client";
